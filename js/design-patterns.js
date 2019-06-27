95c1dc41840e9b38792859434615824665913b15
Robert Resendez <gx2g@gx2g-13.local>
2019-06-27 00:27:15 -0700

    design patterns

--- js/design-patterns.js
+++ js/design-patterns.js
@@ -20,3 +20,17 @@ typeof numberThree; // returns 'number'
 console.log("=============================");
 console.log("numberOne + numberTwo = " + numberThree);
 console.log("=============================");
+
+/* creating objects in JavaScript is trival. This can be seen in the following code for creating an object in JavaScript */
+
+var objectOne = {};
+typeof objectOne // returns 'object'
+var objectTwo = new Object();
+typeof objectTwo; // returns 'object'
+
+/* JavaScript is a dynamic language, adding properties to objects is also quite easy. This can be done even afer the object has been created. The following code creates */
+
+var objectOne = { value : 7}; // object array with perperties declaration
+var objectTwo = {}; // create an empty array object declaration
+objectTwo.value = 8; // use dot notation to assign a value
+
